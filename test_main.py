4ab78db3a5e97dd3f3a0083d6f5044bafb421196
Akash Kamerkar <akash.kamerkar@in.abb.com>
2026-06-30 20:19:07 +0530

    ci: add pipeline and security tests

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_sql_injection_blocked():
+    response = client.get("/users/search?query=' OR 1=1 --")
+    # This should be blocked with a 400, but our vulnerable code
+    # doesn't sanitize input, so it won't behave as expected
+    assert response.status_code == 400
+    
